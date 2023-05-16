48a0f8881790903867d25e4433260e731607d2fa
Maryia Prudnikava <yarosh.m.l@gmail.com>
2023-05-16 10:37:15 +0300

    Пробная структура

---
+++ modules/download_system/database_downloading.py
@@ -0,0 +1,5 @@
+def db_load():
+    return [1, 2, 3] # условный код
+
+def db_clear():
+    return -1 # условный код
---
+++ pipeline.py
@@ -0,0 +1,11 @@
+# --- DOWNLOADING / READING
+
+# --- EDA (разведывательный анализ данных)
+
+# --- FEATURE ENGINEERING
+
+# --- MODELS FITTING (обучение модели)
+
+# --- MODELS VALIDATING
+
+# --- MODELS SAVING
